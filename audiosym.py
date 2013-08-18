6d7ee1bc9756072d253acbde9da274c1fbf9a5c2
pekingking <thepekingking@gmail.com>
2013-08-17 23:35:30 -0400

    included rss index script

--- audiosym.py
+++ audiosym.py
@@ -1,10 +1,9 @@
 import argparse
 import os
-import glob
 import re
 import fnmatch
-
 import requests
+import sys
 
 
 def main():
@@ -12,38 +11,64 @@ def main():
     parser.add_argument("-s", "--source", help="base directory to attach symlinks to")
     parser.add_argument("-d", "--destination", help="base directory /path/to/books/")
     args = parser.parse_args()
+    scriptPath = "{}/{}".format(os.path.dirname(os.path.realpath(__file__)), 'index.txt')
     if not os.path.exists(args.source):
         print "{} is not a valid directory".format(args.source)
-        return
+        sys.exit()
     if not os.path.exists(args.destination):
         print "{} is not a valid directory".format(args.destination)
-        return
-    #TODO remove after debugging
-    if False:
-        print cleanTitle("Harry's big adventure (1asd)")
-    else:
-        #print getSourceFileList(args.source)
-        print "\nsearching for {}".format(os.path.basename(args.source.rstrip('/')))
-        bookIndex = 0
-        bookTitle = os.path.basename(args.source.rstrip('/'))
-        while True:
-            bookDetails = getBookInfo(bookTitle, bookIndex)
-            print "Found {} by {} published in {}".format(bookDetails["title"], bookDetails["author"],
-                                                          bookDetails["date"])
-            correctBook = raw_input("would you like to continue? [yes]/no/next/manual: ")
-            if correctBook == "no":
-                print "sorry we could not find your book"
-                print "exiting"
-                return
-            elif correctBook == "next":
-                bookIndex += 1
-            elif correctBook == "manual":
-                bookTitle = raw_input("what book are you looking for? ")
-                bookIndex = 0
-            elif correctBook == "yes" or correctBook == "":
-                print "great lets continue"
-                break
-        # we have identified the book on books.google.com so we have (title, author, description, date, thumbnail)
+        sys.exit()
+
+    # Identify audiobook and gather book details from books.google.com api.
+    bookDetails = findBookDetails(args.source.rstrip('/'))
+    if not bookDetails:
+        sys.exit()
+    # Verify that the files are in the correct order.
+    # If they are not then the audiobook will be all out of order when we symlink
+    correctOrder = verifyCorrectOrder(args)
+    if not correctOrder:
+        sys.exit()
+    # Show the user a list of the symlinks that are about to be created
+    proceed = actionSummary(args, bookDetails, scriptPath)
+    if not proceed:
+        sys.exit()
+    # Create the symlinks
+    symlinkCreate = createSymlinks(args, bookDetails, scriptPath)
+    if not symlinkCreate:
+        sys.exit()
+    #make sure we are not overwriting something
+
+
+def findBookDetails(source):
+    #print getSourceFileList(args.source)
+    print "\nsearching for {}".format(os.path.basename(source))
+    bookIndex = 0
+    bookTitle = os.path.basename(source)
+    while True:
+        bookDetails = getBookInfo(bookTitle, bookIndex)
+        print "Found {} by {} published in {}".format(bookDetails["title"], bookDetails["author"],
+                                                      bookDetails["date"])
+        correctBook = raw_input("would you like to continue? [yes]/no/next/manual: ")
+        if correctBook == "next":
+            bookIndex += 1
+        elif correctBook == "manual":
+            bookTitle = raw_input("what book are you looking for? ")
+            bookIndex = 0
+        elif correctBook == "yes" or correctBook == "":
+            print "great lets continue"
+            return bookDetails
+        elif correctBook == "no":
+            print "sorry we could not find your book"
+            print "exiting"
+            return
+        else:
+            print "did not recognize your command."
+            print "exiting"
+            return False
+
+
+def verifyCorrectOrder(args):
+    # we have identified the book on books.google.com so we have (title, author, description, date, thumbnail)
         print "\nidentified the following files"
         for bookFile in sorted(getSourceFileList(args.source)):
             print bookFile
@@ -53,42 +78,56 @@ def main():
         if correctOrder == "no":
             print "\nnot available in version 1.0 "
             print "we need to order these before we can fix this"
+            return False
         # the files were in order lets print out what we plan to do
         elif correctOrder == "yes" or correctOrder == "":
-            print "\n"
-            print "planning on creating the following directory and symlinks:"
-            print "source:\t\t\t{}".format(args.source)
-            print "destination:\t{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))
-            print ""
-            print "planning on creating the following symlinks"
-            print "symlink -> file"
+            return True
+
+
+def actionSummary(args, bookDetails, scriptPath):
+    print "\n"
+    print "planning on creating the following directory and symlinks:"
+    print "source:      {}".format(args.source)
+    print "destination: {}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))
+    print ""
+    print "planning on creating the following symlinks"
+    print "symlink -> file"
+    for index, file in enumerate(sorted(getSourceFileList(args.source))):
+        print "{}.%03d{} -> {}".format(cleanTitle(bookDetails["title"]), os.path.splitext(file)[1], file) % (
+            index + 1)
+    print "index.php -> {}".format(scriptPath)
+    proceed = raw_input("Do you want to continue? yes/[no]: ")
+    if proceed == "yes":
+        return True
+    else:
+        return False
+
+
+def createSymlinks(args, bookDetails, scriptPath):
+    if not os.path.exists("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))):
+        print "creating directory"
+        os.mkdir("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"])))
+        if os.path.exists("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))):
+            print "directory created"
+            os.chdir("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"])))
+            print "creating symlinks"
             for index, file in enumerate(sorted(getSourceFileList(args.source))):
-                print "{}.%03d{} -> {}".format(cleanTitle(bookDetails["title"]), os.path.splitext(file)[1], file) % (
-                    index + 1)
-        #TODO create the actual directory and symlinks
-            proceed = raw_input("Do you want to continue? yes/[no]: ")
-            if proceed == "yes":
-                #make sure we are not overwriting something
-                if not os.path.exists("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))):
-                    print "creating directory"
-                    os.mkdir("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"])))
-                    if os.path.exists("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))):
-                        print "directory created"
-                        os.chdir("{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"])))
-                        print "creating symlinks"
-                        for index, file in enumerate(sorted(getSourceFileList(args.source))):
-                            os.symlink("{}/{}".format(args.source.rstrip('/'), file),
-                            "{}/{}/{}.%03d{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]), cleanTitle(bookDetails["title"]), os.path.splitext(file)[1]) % (index + 1))
-                        print "symlinks created"
-                        print "thank you for using audiosym"
-                else:
-                    print "the following directory already exists {}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))
-                    return
+                os.symlink("{}/{}".format(args.source.rstrip('/'), file), "{}/{}/{}.%03d{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]), cleanTitle(bookDetails["title"]), os.path.splitext(file)[1]) % (index + 1))
+            #Symlinks the index.txt to index.php in the new audiobook directory
+            if os.path.exists(scriptPath):
+                os.symlink("{}/{}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]), "index.php"), scriptPath)
+            print "symlinks created"
+            print "thank you for using audiosym"
+            return True
+    else:
+        print "the following directory already exists {}/{}".format(args.destination.rstrip('/'), cleanTitle(bookDetails["title"]))
+        return False
 
 
 def cleanTitle(title):
     """clean up titles so they have no special characters and are . separated"""
     cleanTitle = re.sub("'", "", title)
+    cleanTitle = re.sub(r'(?i)(audiobook|unabridged|abridged)', "", cleanTitle)
     cleanTitle = re.sub("\([^)]+\)", "", cleanTitle)
     cleanTitle = re.sub("\[[^\]]+\]", "", cleanTitle)
     cleanTitle = re.sub("[^a-zA-Z1-9]", ".", cleanTitle)
@@ -106,7 +145,7 @@ def getSourceFileList(directory):
         for dirname, dirnames, filenames in os.walk("./"):
             # print path to all filenames.
             for filename in filenames:
-                if fnmatch.fnmatch(os.path.join(dirname, filename).lstrip("./"), "*.mp3"):
+                if fnmatch.fnmatch(os.path.join(dirname, filename).lstrip("./"), "*.mp3") or fnmatch.fnmatch(os.path.join(dirname, filename).lstrip("./"), "*.m4b") or fnmatch.fnmatch(os.path.join(dirname, filename).lstrip("./"), "*.acc"):
                     fileList.append(os.path.join(dirname, filename).lstrip("./"))
     return fileList
 
